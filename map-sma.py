2a44da435aa9d030d09435188b9b897b65d0fd8f
Atri Basu <atrib3@illinois.edu>
2019-04-01 00:37:50 +0530

    atbasu adding csv file

--- map-sma.py
+++ map-sma.py
@@ -1,6 +1,17 @@
 import sys 
+import datetime
+
+
+smaInterval = sys.argv[1] 
 
 for line in sys.stdin: 
-	val = line.strip() 
-	vals = val.split(',') 
-	print "%s\t%s:%s" % (vals[0], vals[1], vals[2])
+	date,stock,openPrice,closePrice,lowPrice,highPrice = line.strip().split(',')
+	dateD = datetime.datetime.strptime(date, '%m/%d/%Y %H:%M')
+
+	for i in range(0,smaInterval):
+		print "%s\t%s" % ((stock, dateD + datetime.timedelta(days=i),'o'),openPrice)
+		print "%s\t%s" % (stock, dateD + datetime.timedelta(days=i),'c'),closePrice)
+		print "%s\t%s" % (stock, dateD + datetime.timedelta(days=i),'l'),lowPrice)
+		print "%s\t%s" % (stock, dateD + datetime.timedelta(days=i),'h'),highPrice)
+
+
